59d9cdb6634f2c1f0b5c059e3d4d0f4df0120fca
G2Martins <gustavogripaldi@outlook.com>
2026-04-02 13:37:38 -0300

    feat(gestao-usuarios): add GestaoUsuarios component with template, styles, and tests

---
+++ FrontEnd/src/app/features/admin/gestao-usuarios/gestao-usuarios.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-gestao-usuarios',
+    imports: [],
+    templateUrl: './gestao-usuarios.html',
+    styleUrl: './gestao-usuarios.scss',
+})
+export class GestaoUsuarios {}
