a0d0e1d17a08a5d4078e288dbb9289cd5bffeb6b
G2Martins <gustavogripaldi@outlook.com>
2026-04-02 13:37:00 -0300

    feat(pacientes): add CadastroPaciente component with template, styles, and tests

---
+++ FrontEnd/src/app/features/pacientes/cadastro-paciente/cadastro-paciente.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-cadastro-paciente',
+    imports: [],
+    templateUrl: './cadastro-paciente.html',
+    styleUrl: './cadastro-paciente.scss',
+})
+export class CadastroPaciente {}
